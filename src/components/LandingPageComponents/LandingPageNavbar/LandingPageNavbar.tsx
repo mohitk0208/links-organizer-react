462d8ff484ecd81479b1bd8e597f7254788def9e
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-07-16 16:50:59 +0530

    added a Landing page cover file

--- src/components/LandingPageComponents/LandingPageNavbar/LandingPageNavbar.tsx
+++ src/components/LandingPageComponents/LandingPageNavbar/LandingPageNavbar.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import Button from "../utilComponents/Button"
+import Button from "../../utilComponents/Button"
 
 const LandingPageNavbar = () => {
   return (
-    <nav className='flex items-center justify-between px-5 py-3 shadow-sm'>
+    <nav className='flex items-center justify-between px-5 py-3 shadow-md z-10 fixed top-0 left-0 right-0 bg-white'>
       <div>
         <h1 className="font-bold">
           Links Organizer
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import LandingPageNavbar from '../../components/LandingPageNavbar'
+import LandingPageNavbar from '../../components/LandingPageComponents/LandingPageNavbar'
+import LandingPageCover from '../../components/LandingPageComponents/LandingPageNavbar/LandingPageCover'
 
 function LandingPage() {
   return (
     <div className="" >
       <LandingPageNavbar />
+      <LandingPageCover />
     </div>
   )
 }
