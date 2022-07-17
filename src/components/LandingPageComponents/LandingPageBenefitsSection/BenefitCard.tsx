3c48bb71992d646f82fb624aaee879adaf245366
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-07-17 15:04:54 +0530

    added image to benefits

--- src/components/LandingPageComponents/LandingPageBenefitsSection/BenefitCard.tsx
+++ src/components/LandingPageComponents/LandingPageBenefitsSection/BenefitCard.tsx
@@ -11,11 +11,11 @@ interface BenefitCardProps {
 const BenefitCard = ({ title, description, image }: BenefitCardProps) => {
 
   return (
-    <div className='w-56 shadow-sm'>
+    <div className='w-56 '>
       <div className="w-full aspect-video rounded-md overflow-hidden shadow-sm" >
-        <img src={image} className='w-full h-full object-cover' alt="" />
+        <img src={image} className='w-full h-full object-contain' alt="" />
       </div>
-      <div className='text-center px-2'>
+      <div className='text-center px-2 pt-1'>
         <h3 className='font-bold text-xl my-2'>
           {title}
         </h3>
