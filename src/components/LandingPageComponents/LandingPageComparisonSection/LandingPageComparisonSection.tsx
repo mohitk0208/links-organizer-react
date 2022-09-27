3a9a8078afd71cdc05b68e2d56904c810547c72a
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-09-27 13:17:18 +0530

    bug fixes related to key attribute and table tags
    that `tr can not be the direct child of table tag, so to use thead, tbody, and tfoot tags

--- src/components/LandingPageComponents/LandingPageComparisonSection/LandingPageComparisonSection.tsx
+++ src/components/LandingPageComponents/LandingPageComparisonSection/LandingPageComparisonSection.tsx
@@ -50,7 +50,7 @@ const LandingPageComparisonSection = () => {
           <HeadingRow col1="Feature" col2="Links Organizer" col3="Bookmarking" />
 
           {comparisonPoints.slice(0, comparisonPoints.length - 1).map(point => (
-            <NormalRow {...point} />
+            <NormalRow key={point.col1} {...point} />
           ))}
 
           <LastRow {...comparisonPoints[comparisonPoints.length - 1]} />
@@ -66,41 +66,47 @@ const LandingPageComparisonSection = () => {
 
 const HeadingRow = ({ col1, col2, col3 }: RowProps) => {
   return (
-    <tr className="border-b bg-purple-100 rounded-t-lg">
-      <th className="border-b border-r text-left px-8 py-4">{col1}</th>
-      <th className="border-b border-r text-left px-8 py-4">{col2}</th>
-      <th className="border-b text-left px-8 py-4">{col3}</th>
-    </tr>
+    <thead>
+      <tr className="border-b bg-purple-100 rounded-t-lg">
+        <th className="border-b border-r text-left px-8 py-4">{col1}</th>
+        <th className="border-b border-r text-left px-8 py-4">{col2}</th>
+        <th className="border-b text-left px-8 py-4">{col3}</th>
+      </tr>
+    </thead>
   )
 }
 
 const NormalRow = ({ col1, col2, col3 }: RowProps) => {
   return (
-    <tr>
-      <td className=" border-b border-r px-8 py-4">{col1}</td>
-      <td className=" border-b border-r px-8 py-4">
-        {col2}
-      </td>
-      <td className=" border-b px-8 py-4">
-        {col3}
-      </td>
-    </tr>
+    <tbody>
+      <tr>
+        <td className=" border-b border-r px-8 py-4">{col1}</td>
+        <td className=" border-b border-r px-8 py-4">
+          {col2}
+        </td>
+        <td className=" border-b px-8 py-4">
+          {col3}
+        </td>
+      </tr>
+    </tbody>
   )
 }
 
 const LastRow = ({ col1, col2, col3 }: RowProps) => {
   return (
-    <tr>
-      <td className="border-r px-8 py-4">
-        {col1}
-      </td>
-      <td className="border-r px-8 py-4">
-        {col2}
-      </td>
-      <td className=" px-8 py-4">
-        {col3}
-      </td>
-    </tr>
+    <tfoot>
+      <tr>
+        <td className="border-r px-8 py-4">
+          {col1}
+        </td>
+        <td className="border-r px-8 py-4">
+          {col2}
+        </td>
+        <td className=" px-8 py-4">
+          {col3}
+        </td>
+      </tr>
+    </tfoot>
   )
 }
 
