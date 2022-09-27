5e9e01834f2483a6382933856561c27acf6e0e69
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-09-27 13:27:09 +0530

    removed date-fns

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Tag from '../utilComponents/Tag'
 import { ShareIcon, PencilIcon, TrashIcon } from "@heroicons/react/outline"
-// import { format, isToday, isYesterday } from 'date-fns'
 import Modal from '../utilComponents/Modal'
 import DeleteConfirmModal from '../DeleteConfirmModal'
 import { deleteLinkAsync } from '../../slices/linksSlice'
