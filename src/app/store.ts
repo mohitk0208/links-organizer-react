af536ec36f7af8f14d0a98600e736148839590a4
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-09-27 13:09:10 +0530

    upgraded to @reduxjs/toolkit@1.8.5

--- src/app/store.ts
+++ src/app/store.ts
@@ -22,8 +22,8 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+export type AppThunk = ThunkAction<void, RootState, undefined, Action<string>>;
 
 export default store
